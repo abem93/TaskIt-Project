e58c3ae442c3ef974f66b4a908190ffd28e00897
Abraham Mejia <aimejia4@gmail.com>
2023-10-09 20:27:42 -0500

    Created and styled sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { ProfileComponent } from './shared/sidebar/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TaskListComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
