5c8a1439e85d275b61e12127d44f784e638b958a
Abe <abe@Dess-MacBook-Air.local>
2023-10-19 20:27:13 -0500

    Added filters selection and Add new Task button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { TaskListComponent } from './task-list/task-list.component';
 import { ProfileComponent } from './shared/sidebar/profile/profile.component';
+import { TaskModalComponent } from './task-list/task-modal/task-modal.component';
+import { TaskListComponent } from './task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    TaskListComponent,
-    ProfileComponent
+    ProfileComponent,
+    TaskModalComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/task-list/task-modal/task-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-modal',
+  templateUrl: './task-modal.component.html',
+  styleUrls: ['./task-modal.component.css']
+})
+export class TaskModalComponent {
+
+}
